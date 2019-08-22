fbeeaf3fc77da7b9881d50bfa13bd20779786287
9992 <hansol416@naver.com>
2019-08-22 17:53:42 +0900

    14323456543245676543

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// var express = require('express');
-// var app = express();
-// var router = require('./router/main')(app);
+var express = require('express');
+var app = express();
+var router = require('./router/main')(app);
 
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-// app.engine('html', require('ejs').renderFile);
+app.set('src', __dirname + '/src');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-// var server = app.listen(3000, function(){
-//     console.log("Express server has started on port 3000")
-// });
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+});
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-console.log("그냥 헤로쿠 콘솔에 찍히는지 확인하려고 하는 테스트 입니다.")
+console.log("그냥 헤로쿠 콘솔에 찍히는지 확인하려고 하는 테스트 입")
