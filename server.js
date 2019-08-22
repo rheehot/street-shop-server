871cd5701576aabfc106060d3267b0d31b1e1f96
9992 <hansol416@naver.com>
2019-08-22 18:05:16 +0900

    somebody helpme

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var router = require('./router/main')(app);
 
-app.set('src', __dirname + '/src');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
