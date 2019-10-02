eb1aa0343ca2a428f9ba6f4748dddf85027d90ec
ellapresso <ellapresso@gmail.com>
2019-10-02 16:27:26 +0900

    주석추가

--- server/api/users/controllers/join.js
+++ server/api/users/controllers/join.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose')
 const users = require('../model/Users')
 
 async function join(req, res) {
-    // const test = '1234'
+    // 로컬에 만들어 놓은 books라는 컬렉션을 불러옴.
     const test = await users.find().exec().catch(err => console.log(err));
     console.log(test)
     res.send(test)
-
-    //디비연결을 해본다.
-    //localhost:30000/api/users/ 에서 몽구스 연결된것을 확인할 수 있는 페이지 만들기
+    //localhost:port/api/users 에서 확인 가능.
 }
 
 
