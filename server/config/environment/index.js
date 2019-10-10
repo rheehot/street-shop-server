c7c0ea1b42db1172ee112189314a69dfd81deaf3
ellapresso <ellapresso@gmail.com>
2019-10-10 17:51:00 +0900

    port처리

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -10,22 +10,22 @@ dotenv.config({
 // mongodb://<dbuser>:<dbpassword>@ds329058.mlab.com:29058/heroku_dmhkbx4g
 /* istanbul ignore next */
 const config = {
-  //TODO 바꿔라
-  env: process.env.NODE_ENV || 'development',
-  root: path.normalize(`${__dirname}/../../..`),
-  tz: process.env.TZ || 'Asia/Seoul',
-  ip: process.env.IP || '0.0.0.0',
-  database: {
-    mongoosedb: {
-      dialect: process.env.SSS_DATABASE_TYPE || 'mongodb',
-      host: process.env.SSS_DATABASE_HOST || '',
-      port: process.env.SSS_DATABASE_PORT || 29058,
-      username: process.env.SSS_DATABASE_USERNAME || 'localhost',
-      password: process.env.SSS_DATABASE_PASSWORD || '',
-      schema: process.env.SSS_DATABASE_SCHEMA||'',
-      dbname: process.env.SSS_DATABASE_DBNAME||'',
-      path: process.env.SSS_DATABASE_PATH||'',
-      },
+    //TODO 바꿔라
+    env: process.env.NODE_ENV || 'development',
+    root: path.normalize(`${__dirname}/../../..`),
+    tz: process.env.TZ || 'Asia/Seoul',
+    ip: process.env.IP || '0.0.0.0',
+    database: {
+        mongoosedb: {
+            dialect: process.env.SSS_DATABASE_TYPE || 'mongodb',
+            host: process.env.SSS_DATABASE_HOST || '',
+            port: process.env.SSS_DATABASE_PORT || '',
+            username: process.env.SSS_DATABASE_USERNAME || 'localhost',
+            password: process.env.SSS_DATABASE_PASSWORD || '',
+            schema: process.env.SSS_DATABASE_SCHEMA || '',
+            dbname: process.env.SSS_DATABASE_DBNAME || '',
+            path: process.env.SSS_DATABASE_PATH || '',
+        },
     },
 };
 
