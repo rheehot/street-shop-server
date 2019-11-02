a1b666df2b90c5d716516706dc649fe60571e1ee
ellapresso <ellapresso@gmail.com>
2019-11-02 12:33:40 +0900

    오타수정

--- server/api/shops/controllers/list.js
+++ server/api/shops/controllers/list.js
@@ -1,10 +1,9 @@
 const shops = require('../model/shops')
 
 async function list(req, res) {
-    const test = await shops.find().exec().catch(err => console.log(err));
-    console.log(test)
-    res.send(test)
-    //localhost:port/api/test 에서 확인 가능.
+    const list = await shops.find().exec().catch(err => console.log(err));
+
+    res.send(list)
 }
 
 module.exports = list;
--- server/api/users/controllers/join.js
+++ server/api/users/controllers/join.js
@@ -2,9 +2,7 @@ const Users = require('../model/Users')
 
 async function join(req, res) {
     const test = await Users.find().exec().catch(err => console.error(err));
-    console.log(test)
     res.send(test)
-    //localhost:port/api/test 에서 확인 가능.
 }
 
 module.exports = join;
