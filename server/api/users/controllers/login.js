fd6ac809299fb2efa6ada557af60055e4d20181f
ellapresso <ellapresso@gmail.com>
2019-11-30 13:55:22 +0900

    todo

--- server/api/users/controllers/login.js
+++ server/api/users/controllers/login.js
@@ -1,28 +1,23 @@
-// const User = require('../model/Users');
+const User = require('../model/Users');
 const axios = require('axios');
 
 function kakaoLogin(req, res){
-    const { token } = req.body;
-    console.log(req)
+    const { token } = req.body; //프론트에서 넘겨주는 회원 엑세스 토큰
+
     return axios({
         method: 'post',
         url: 'https://kapi.kakao.com/v2/user/me',
-        headers: 'njb6idcF12A38hByG8N_gvvogALfUoQwsUSej1go9dJcAAAFuuoRpPA',
+        headers: token,
         responseType: 'json',
     }).then(function (response) {
         console.log(response.data);
-        // const id = response.data.id;
-        // const nickname = response.data.properties.nickname;
-        // const profile_image = response.data.properties.profile_image;
-        // const thumbnail_image = response.data.properties.thumbnail_image;
-
-        // const userinfo = [id, nickname, profile_image, thumbnail_image, nickname, profile_image, thumbnail_image];
-        // const userJoin = USER.saveUser(userinfo);
+        //TODO: response.data.id ( 회원 고유 아이디 )가 디비에 있는지 확인한다.
+        // 있으면 flag 가입유무를 확인해서 가입된 유저이면 로그인, 200
+        // 가입되어있지 않은 회원이면 = 401error
+        // 아이디가 아예 없으면 디비에 저장후 = 404 error
         res.send(200, {
-            // userJoin,
-            // status: 'ok',
+            //아이디 필수 반환
         });
-        res.send(response.data)
     })
 }
 
--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const server = http.createServer(app);
 
 // body-parser, post 요청시 body 데이터 추출 하기 위함
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // 라우트 설정
 api.route(app);
@@ -33,14 +34,14 @@ process.on("uncaughtException", err => {
  *  - 실시간 서버 연결 종료
  *  - 데이터베이스 커넥션 종료
  */
-// process.on("SIGINT", () => {
-//   server.close(() => {
-//     console.info("APP", "close.");
-//     //TODO 디비연결해제부분
-//     // database.mongooseDb.close();
-//     process.exit(0);
-//   });
-// });
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.info("APP", "close.");
+    //TODO 디비연결해제부분
+    database.mongooseDb.close();
+    process.exit(0);
+  });
+});
 
 server.listen(config.port, () => {
     console.info('[APP]', `listening on port ${config.port}.`);
