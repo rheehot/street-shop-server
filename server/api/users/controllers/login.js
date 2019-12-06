698719ace350c419aa0afdd7d29d4d9924226383
ellapresso <ellapresso@gmail.com>
2019-12-06 20:26:58 +0900

    로그인 api
    가입된 회원인지 판별후, 유저이면 로그인, 아니면 정보를 저장한다.

--- server/api/users/controllers/login.js
+++ server/api/users/controllers/login.js
@@ -10,26 +10,42 @@ function kakaoLogin(req, res){
         headers: {'Authorization':token},
         responseType: 'json',
     }).then(async function(response) {
+        const user = response.data;
+        const userId = user.id;
         //로그인한 유저가 가입된 유저인지(또는 유효한 회원인지) 확인
-        const flag = await Users.findOne({ userId : response.data.id, isUser : true });
-        if (!flag) {
-            return res.sendStatus(404);
+        const isUser = await Users.findOne({ userId : user.id, isUser : true });
+        if (!isUser) {
+            const userData = {
+                kakao: {
+                    active: false,
+                    nickname: user.properties.nickname,
+                    profileLink: user.properties.thumbnail_image
+                },
+            }
+            //upsert: 가입하지않았지만 이미 정보가 있는 경우 update, 없는경우 insert해준다..
+            await Users.findOneAndUpdate({ userId }, userData, { upser: true });
+            return res.send({ isUser: false, userId });
         }
 
         //TODO: 데이터 상의후(회원가입 완료후) 다듬어야 함
         //회원일 경우 유저정보 반환
-        const user = response.data;
         const userInfo = {
-            uid: user.id,
+            userId,
             nickname: user.properties.nickname,
-            image: user.properties.thumbnail_image,
-            isOwner: flag.owner,
+            profileLink: user.properties.thumbnail_image,
+            isOwner: isUser.owner,
+            isUser: true
         }
-        if(!flag.kakao.active){
-            userInfo.nickname = flag.nickName;
+        //새로 로그인 한 경우 새로운 데이터로 저장
+        isUser.set('kakao.nickname', user.properties.nickname);
+        isUser.set('kakao.profileLink', user.properties.thumbnail_image);
+        isUser.save();
+
+        if (!isUser.kakao.active) {
+            userInfo.nickname = isUser.nickname;
         }
-        return res.send(userInfo)
-    }).catch(err=>console.error(`[login error] ${err}`))
+        return res.send(userInfo);
+    }).catch((err) => console.error(`[login error] ${err}`));
     
 }
 
--- server/api/users/model/Users.js
+++ server/api/users/model/Users.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const UsersSchema = new mongoose.Schema({
-    nickName: String,
-    userId: String,
-    userTags: Array,
+    nickname: String,
+    userId: { type:String, required: true },
+    userTags: {type: Array, default: []},
     userPasswords: String,
-    userFavorites: Array,
-    kakao : Object,
-    owner : Boolean
+    userFavorites: {type: Array, default: []},
+    kakao : {type: Object, default: {}},
+    owner : {type: Boolean, default: false },
+    isUser: { type: Boolean, default: false }, //가입 정보
+    deleted: { type: Boolean, default: null } //탈퇴 정보
 });
 
 global.Users = global.Users || mongoose.model('users', UsersSchema);
