70715dc797eb3d9e50b5b0b8ec7f893528b09fdc
ellapresso <ellapresso@gmail.com>
2019-12-06 19:33:05 +0900

    카카오에 유저정보 요청 진행중

--- server/api/users/controllers/login.js
+++ server/api/users/controllers/login.js
@@ -2,23 +2,36 @@ const Users = require('../model/Users');
 const axios = require('axios');
 
 function kakaoLogin(req, res){
-    const { token } = req.body; //프론트에서 넘겨주는 회원 엑세스 토큰
+    const token = req.headers.authorization;
+
     return axios({
-        method: 'post',
+        method: 'get',
         url: 'https://kapi.kakao.com/v2/user/me',
-        headers: {'Authorization' : `Bearer ${token}`},
+        headers: {'Authorization':token},
         responseType: 'json',
-        mode: 'cors', //toUpper error fix
-    }).then(function (response) {
-        const flag = Users.findOne({userId : response.data.id})
-        console.log(flag._conditions.userId)
-        // TODO: response.data.id ( 회원 고유 아이디 )가 디비에 있는지 확인한다.
-        // Error : Error 발생 시 then 에서 중단되어 버려 내부에서 401, 404 error로 분류할 수 없는 문제 발생
-        // 있으면 flag 가입유무를 확인해서 가입된 유저이면 로그인, 200
-        // 가입되어있지 않은 회원이면 = 401error
-        // 아이디가 아예 없으면 디비에 저장후 = 404 error
-        res.send(200,response.data.id);
-    })
+    }).then(async function(response) {
+        //로그인한 유저가 가입된 유저인지(또는 유효한 회원인지) 확인
+        const flag = await Users.findOne({ userId : response.data.id, isUser : true });
+        if (!flag) {
+            //회원이 아닐경우
+            return res.send('회원가입을 시키자.')
+        }
+        //TODO: 데이터 상의후(회원가입 완료후) 다듬어야 함.
+        const user = response.data;
+        const userInfo = {
+            uid: user.id,
+            nickname: user.properties.nickname,
+            image: user.properties.thumbnail_image,
+            isOwner: flag.owner,
+        }
+
+        if(!flag.kakao.active){
+            userInfo.nickname = flag.nickName;
+        }
+
+        return res.send(userInfo)
+    }).catch(err=>console.log(err))
+    
 }
 
 module.exports = kakaoLogin;
