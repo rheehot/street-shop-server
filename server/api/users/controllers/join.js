baf1c6afd618d7fa1577135feb3ba6e3dad402aa
9992 <hansol416@tu.ac.kr>
2019-12-21 10:29:23 +0900

    회원가입(사장, 일반) 완료
    Users 부분 스키마 변경

--- server/api/users/controllers/join.js
+++ server/api/users/controllers/join.js
@@ -1,31 +1,29 @@
 const Users = require('../model/Users')
+// async function join(req, res) {
+//     // Array 요소 : userTags, userFavorites, String 요소 : userId, Passwords(해시 암호화 필요), Object 요소 : Kakao, 
+//     const joinInfo = {
+//         userId: req.body.userId,
+//         nickName: req.body.nickName,
+//         userTags: req.body.userTags,
+//         userPasswords: req.body.userPasswords,
+//         userFavorites: req.body.userFavorites,
+//         kakao : req.body.kakao,
+//         owner : req.body.owner
+//     }
+//     const join = await Users.create(joinInfo);
+//     res.send(join)
+// }
 
-async function join(req, res) {
-    // Array 요소 : userTags, userFavorites, String 요소 : userId, Passwords(해시 암호화 필요), Object 요소 : Kakao, 
-    const joinInfo = {
-        userId: req.body.userId,
-        nickName: req.body.nickName,
-        userTags: req.body.userTags,
-        userPasswords: req.body.userPasswords,
-        userFavorites: req.body.userFavorites,
-        kakao : req.body.kakao,
-        owner : req.body.owner
-    }
-    const join = await Users.create(joinInfo);
-    res.send(join)
-}
-
-
-// TODO: 이미 저장되어있는 유저정보에서 추가 입력받은 데이터를 업데이트한다.
+// TODO: 이미 저장되어 있는 유저정보에서 추가 입력받은 데이터를 업데이트한다.
 // 일반유저와 사장유저일 경우의 두가지 경우로 구분한다.
-function classifyJob(req, res){
-    const isOwner = req.params.isOwner;
+async function join(req, res){
+    const isOwner = req.body.isOwner;
     if(isOwner===true){
         // 사장일 경우 userId, owner : true , nickname, shop정보
             // shop정보: { 사장이름, 가게이름, 운영요일: [], 가게태그: [], 운영시작시간: '', 운영종료시간: '', 운영위치: {경도:'', 위도: ''}}
         const ownerData = {
             userId: req.body.userId,
-            owner : req.body.owner,
+            owner : isOwner,
             nickName: req.body.nickName,
             shopInfo: {
                 ownerName: req.body.ownerName,
@@ -36,16 +34,18 @@ function classifyJob(req, res){
                 latitude: req.body.latitude            
             }
         }
-        await Users.create(ownerData)
+        const joinData = await Users.create(ownerData)
+        res.send(`${joinData.nickName}님 환영합니다.`).status(200)
         } else {
         // 일반일 경우 userId, owner : false , nickname, 관심태그:[]
         const userData = {
             userId: req.body.userId,
-            owner : req.body.owner,
+            owner : isOwner,
             nickName: req.body.nickName,
             userFavorites: req.body.userFavorites
         }
-        await Users.create(userData);
+        const joinData = await Users.create(userData)
+        res.send(`${joinData.nickName}님 환영합니다.`).status(200)
     }
 }
 
--- server/api/users/model/Users.js
+++ server/api/users/model/Users.js
@@ -1,18 +1,24 @@
 const mongoose = require('mongoose');
 
 const UsersSchema = new mongoose.Schema({
-    nickname: String,
+    nickName: String,
     userId: { type:String, required: true },
     userTags: {type: Array, default: []},
     userPasswords: String,
     userFavorites: {type: Array, default: []},
     kakao : {type: Object, default: {}},
     owner : {type: Boolean, default: false },
+    onwerName : String,
+    shopName : String,
+    openTime: Date,
+    closeTime: Date,
+    longitude : mongoose.Types.Decimal128, 
+    latitude : mongoose.Types.Decimal128,
+    shopInfo: Object,
     isUser: { type: Boolean, default: false }, //가입 정보
     deleted: { type: Boolean, default: null } //탈퇴 정보
 });
 
 global.Users = global.Users || mongoose.model('users', UsersSchema);
 const Users = mongoose.model('users', UsersSchema);
-
 module.exports = Users;
