5e3560a543e3fc1739d3c5aa7c57a75291f70f28
ellapresso <ellapresso@gmail.com>
2019-12-29 17:14:29 +0900

    유효성검사 추가

--- server/module/oAuth.js
+++ server/module/oAuth.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const tokenCheck = (token) => {
+	if(!token) return 403;
 	return axios({
 		methoe: 'get',
 		url: 'https://kapi.kakao.com/v1/user/access_token_info',
