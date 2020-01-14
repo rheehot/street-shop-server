199c8b2c8c5d8731f97d0c0dc3ac28c219fdc2d0
ellapresso <ellapresso@gmail.com>
2020-01-14 20:25:19 +0900

    아이콘(태스) 리스트

--- server/api/tags/controllers/list.js
+++ server/api/tags/controllers/list.js
@@ -1,5 +1,11 @@
-function list(){
+const Tags = require('../model/Tags');
 
+async function list(req,res){
+	const data = await Tags.find();
+	const result = data.map((e)=>{
+		return {icon : e.icon, tags:e.tags};
+	})
+	return res.send(result)
 }
 
 module.exports = list;
